7d38b5da48f618ee66571d5675f0574641edeac7
brunoascenc <brunoascenc1@outlook.com>
2021-03-01 20:30:36 -0300

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,53 +1,82 @@
-import React from 'react';
-import { AiFillLinkedin } from "react-icons/ai";
+import React from "react";
+import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
 import { GrMail } from "react-icons/gr";
 import styled from "styled-components";
 
-const FooterContainer = styled.footer `
-background: #36325c;
-height: 300px;
-display: flex;
-align-items: center;
-`
+const FooterContainer = styled.footer`
+  background: #2c2638;
+  color: #d6d3db;
+  height: 300px;
+  display: flex;
+  /* justify-content: space-between; */
+  align-items: center;
+`;
 
-const ContactInfo = styled.div `
-
-display: flex;
-flex-direction: column;
-color: #f0e5e5;
-padding-left: 5%;
-padding-right: 5%;
-h2{
+const ContactInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-left: 5%;
+  padding-right: 5%;
+  h2 {
     text-transform: uppercase;
     margin-bottom: 10px;
-    font-size: 2.3rem;
-}
+    font-size: 1.5rem;
+    margin-bottom: 20px;
+    letter-spacing: 1px;
+  }
 
-p{
+  .name{
+      font-size: 1.5rem;
+  }
+
+  p {
     font-size: 1.2rem;
     margin-bottom: 10px;
+    line-height: 29px;
+  }
+
+  div {
+    display: flex;
+    /* align-items: center; */
+    /* margin-bottom: 5px; */
+    font-size: 1.3rem;
+
+    p{
+        margin-left: 5px;
+    }
+  }
+`;
+
+const Copy = styled.div `
+p{
+    font-size: 1.5rem;
 }
 
-span{
-    /* display: flex;
-    align-items: center; */
-}
 
 `
 
 const Footer = () => {
-    return (
-        <FooterContainer>
-            <ContactInfo>
-                <h2>Contact Info</h2>
-                <p>Bruno Ascenção <br/> São Paulo, SP</p>
-                <span><GrMail/> brunoascencao1@outlook.com</span>
-                <span><AiFillLinkedin/> Linkedin</span>
+  return (
+    <FooterContainer>
+      <ContactInfo>
+        <h2>Contact</h2>
+        <p>
+          <span className="name">Bruno Ascenção</span> <br /> São Paulo, SP
+        </p>
+        <div>
+          <GrMail size={25} /> <p>brunoascencao1@outlook.com</p>
+        </div>
 
-            </ContactInfo>
-            
-        </FooterContainer>
-    )
-}
+        <div>
+          <AiFillLinkedin size={25} /> <p>Linkedin</p>
+        </div>
+      </ContactInfo>
+      <Copy>
+          <p><AiFillGithub/> Website repository</p>
+          <span>© copyright 2021 Bruno Ascenção</span>
+      </Copy>
+    </FooterContainer>
+  );
+};
 
-export default Footer
+export default Footer;
